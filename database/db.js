5c59e5810adf3b8b615b682a99edfa4590b13341
Daksh-14 <dakshmv14@gmail.com>
2025-04-11 14:43:52 +0530

    Changed database

--- database/db.js
+++ database/db.js
@@ -1,5 +1,15 @@
 import pg from 'pg';
-import 'dotenv/config'
+import 'dotenv/config';
+import fs from 'fs';
+
 export const db = new pg.Client({
-    connectionString: process.env.connectionString
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: {
+    ca: fs.readFileSync(process.env.CA_CERT).toString(),
+    rejectUnauthorized: true
+  }
 });
